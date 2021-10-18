1b525f1d7857fae95af7a58e0a52c30a8d4213be
breadA <dhairy.srivastava@gmail.com>
2021-10-18 05:14:08 +0000

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<main class="flex items-center justify-center h-screen bg-gray-800 gap-x-12">
+	<h1 class="font-semibold text-3xl">Woof woof, here is the artwork for frostERP</h1>
+	<div class="w-screen h-screen">
+		<img alt="artwork #1" />
+	</div>
+</main>
